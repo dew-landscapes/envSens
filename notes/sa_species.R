8834d693f9ebace4d540ab80a2841ffde37ba997
Eryn Chang <EChang@env.sa.gov.au>
2026-02-06 13:14:28 +1030

    removed some dir

--- notes/sa_species.R
+++
@@ -1,8 +0,0 @@
-
-## Read all the species in SA
-tarchetypes::tar_file_read(name = sa_species,
-                           command = fs::path(tars$clean$store, "objects", "bio_clean"),
-                           read = arrow::open_dataset(!!.x) %>% 
-                             filter(grepl(" ", taxa)) %>% 
-                             distinct() %>% 
-                             collect())
